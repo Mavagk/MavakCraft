f78d5cb3c0458e49d9d8b9054fa92eaf6494b8c3
SwampMaster2160 <jaxon1454@gmail.com>
2024-12-26 17:00:37 +1300

    Setup

---
+++ src/main/java/net/mavakcraft/MavakCraft.java
@@ -0,0 +1,28 @@
+package net.mavakcraft;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+
+@Mod(MavakCraft.MODID)
+public class MavakCraft
+{
+	// Define mod id in a common place for everything to reference
+	// The value here should match an entry in the META-INF/neoforge.mods.toml file
+	public static final String MODID = "mavakcraft";
+	// The logger to log debug messages to
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	// The constructor for the mod class is the first code that is run when your mod is loaded.
+	// FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+	public MavakCraft(IEventBus modEventBus, ModContainer modContainer) {
+		LOGGER.info("Loading MavakCraft");
+		// Register our mod's ModConfigSpec so that FML can create and load the config file for us
+		modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+	}
+}
