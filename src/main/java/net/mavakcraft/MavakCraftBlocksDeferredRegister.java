d73490a5476b394b242867285033824420a68d62
SwampMaster2160 <jaxon1454@gmail.com>
2024-12-27 18:24:03 +1300

    Work on custom block register

---
+++ src/main/java/net/mavakcraft/MavakCraftBlocksDeferredRegister.java
@@ -0,0 +1,9 @@
+package net.mavakcraft;
+
+import net.neoforged.neoforge.registries.DeferredRegister.Blocks;
+
+public class MavakCraftBlocksDeferredRegister extends Blocks {
+	protected MavakCraftBlocksDeferredRegister(String namespace) {
+		super(namespace);
+	}
+}
