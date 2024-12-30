db7b7978a1a7c065a8c892db4f3a1980048a86a1
SwampMaster2160 <jaxon1454@gmail.com>
2024-12-30 13:09:38 +1300

    Add loot table providers for blocks

--- src/main/java/net/mavakcraft/DataGenerators.java
+++ src/main/java/net/mavakcraft/DataGenerators.java
@@ -8,24 +8,26 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Generates data such as block drop loot tables, models and blockstates for us when `runData` is run.
+ */
 @EventBusSubscriber(modid = MavakCraft.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
 		DataGenerator generator = event.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
-		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+		//ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
 		generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
-			List.of(new LootTableProvider.SubProviderEntry(BlockLootProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+			List.of(new LootTableProvider.SubProviderEntry(BlockLootProvider::new, LootContextParamSets.BLOCK)), lookupProvider
+		));
 	}
 }
--- src/main/java/net/mavakcraft/datagenerator/BlockLootProvider.java
+++ src/main/java/net/mavakcraft/datagenerator/BlockLootProvider.java
@@ -9,14 +9,19 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
 
+/**
+ * Generates loot tables for blocks.
+ */
 public class BlockLootProvider extends BlockLootSubProvider {
-	protected BlockLootProvider(HolderLookup.Provider registries) {
+	public BlockLootProvider(HolderLookup.Provider registries) {
 		super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
 	}
 
 	@Override
 	protected void generate() {
-		throw new UnsupportedOperationException("Unimplemented method 'generate'");
+		dropSelf(MavakCraft.GLOWING_OBSIDIAN.get());
+		dropSelf(MavakCraft.ROSE.get());
+		dropSelf(MavakCraft.BLUE_ROSE.get());
 	}
 
 	@Override
