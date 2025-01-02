9d24cc89b2308a35d00581cd43b62874c70a82ef
SwampMaster2160 <jaxon1454@gmail.com>
2025-01-02 17:05:18 +1300

    Work on byte blocks

---
+++ src/main/java/net/mavakcraft/block/AbstractByteBlock.java
@@ -0,0 +1,36 @@
+package net.mavakcraft.block;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.Direction;
+
+public abstract class AbstractByteBlock extends Block {
+	public AbstractByteBlock(BlockBehaviour.Properties properties) {
+		super(properties);
+	}
+
+	/**
+	 * Get the byte value of the current block or {@link null} if the block doesn't have a byte value.
+	 */
+	public @Nullable Integer getByteValue(BlockState state) {
+		return null;
+	}
+
+	/**
+	 * Get the byte value of a connecting block.
+	 * @param pos The position of the current block (NOT the connecting block we want to get the value of).
+	 * @param direction The direction from the current block to the connecting block.
+	 * @return The byte value of the connecting block or {@link null} if it doesn't have a byte value or doesn't extend {@link AbstractByteBlock}.
+	 */
+	public @Nullable Integer getByteValueOfConnectingBlockBlock(LevelReader level, BlockPos pos, Direction direction) {
+		BlockState connectionBlockState = level.getBlockState(pos.offset(direction.getNormal()));
+		Block connectionBlock = connectionBlockState.getBlock();
+		if (!(connectionBlock instanceof AbstractByteBlock)) return null;
+		return ((AbstractByteBlock)connectionBlock).getByteValue(connectionBlockState);
+	}
+}
---
+++ src/main/java/net/mavakcraft/block/ByteBlock.java
@@ -0,0 +1,29 @@
+package net.mavakcraft.block;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class ByteBlock extends AbstractByteBlock {
+	public static final IntegerProperty BYTE_VALUE = IntegerProperty.create("value", 0, 255);
+
+	public ByteBlock(BlockBehaviour.Properties properties) {
+		super(properties);
+	}
+
+	@Override
+	protected void createBlockStateDefinition(@Nonnull StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(BYTE_VALUE);
+		this.registerDefaultState(this.stateDefinition.any().setValue(BYTE_VALUE, Integer.valueOf(0)));
+	}
+
+	@Override
+	public @Nullable Integer getByteValue(BlockState state) {
+		return state.getValue(BYTE_VALUE);
+	}
+}
