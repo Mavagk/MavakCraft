4e9e6db77ba5814101faafee10dae2d7c1bfd865
SwampMaster2160 <jaxon1454@gmail.com>
2025-02-17 17:31:27 +1300

    Change water sim block

--- src/main/java/net/mavakcraft/block/simblock/WaterSimBlock.java
+++ src/main/java/net/mavakcraft/block/simblock/WaterSimBlock.java
@@ -3,6 +3,7 @@ package net.mavakcraft.block.simblock;
 import javax.annotation.Nonnull;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
@@ -22,6 +23,17 @@ public class WaterSimBlock extends AbstractSimBlock {
 		if (level.getBlockState(posBelow).isAir()) {
 			level.setBlockAndUpdate(posBelow, state);
 			level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+			return;
+		}
+		int flowDirection = random.nextInt(4);
+		for (int x = 0; x < 4; x++) {
+			flowDirection = (flowDirection + 1) % 4;
+			BlockPos flowToPos = pos.offset(Direction.BY_ID.apply(flowDirection + 2).getNormal());
+			if (level.getBlockState(flowToPos).isAir()) {
+				level.setBlockAndUpdate(flowToPos, state);
+				level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+				return;
+			}
 		}
 	}
 
