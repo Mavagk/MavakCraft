41d2fea6142eff3cc9240aba3337ee4d38bc6686
SwampMaster2160 <jaxon1454@gmail.com>
2025-02-17 13:57:18 +1300

    Work on water block

--- src/main/java/net/mavakcraft/block/simblock/WaterSimBlock.java
+++ src/main/java/net/mavakcraft/block/simblock/WaterSimBlock.java
@@ -1,7 +1,39 @@
 package net.mavakcraft.block.simblock;
 
+import javax.annotation.Nonnull;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
 public class WaterSimBlock extends AbstractSimBlock {
 	public WaterSimBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	protected void tick(@Nonnull BlockState state, @Nonnull ServerLevel level, @Nonnull BlockPos pos, @Nonnull RandomSource random) {
+		super.tick(state, level, pos, random);
+		BlockPos posBelow = pos.below();
+		if (level.getBlockState(posBelow).isAir()) {
+			level.setBlockAndUpdate(posBelow, state);
+			level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+		}
+	}
+
+	@Override
+	protected void onPlace(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, @Nonnull BlockState oldState, boolean movedByPiston) {
+		super.onPlace(state, level, pos, oldState, movedByPiston);
+		level.scheduleTick(pos, state.getBlock(), 0);
+	}
+	
+	@Override
+	protected void neighborChanged(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, @Nonnull Block neighborBlock, @Nonnull BlockPos neighborPos, boolean movedByPiston) {
+		super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
+		level.scheduleTick(pos, state.getBlock(), 0);
 	}
 }
